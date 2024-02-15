e095ff59086cc617043707c60ea0b4ee41864853
MrVoodoPeople <mrvoodopeople@gmail.com>
2024-02-15 13:18:26 +0300

    UnitTest
    PythonUnitTest

---
+++ Python/UnitTestIsPrime.py
@@ -0,0 +1,25 @@
+import unittest
+
+def isprime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+class TestIsprime(unittest.TestCase):
+    def test_isprime(self):
+        self.assertTrue(isprime(53))
+        self.assertTrue(isprime(3))
+        self.assertTrue(isprime(5))
+        self.assertTrue(isprime(7))
+        self.assertFalse(isprime(4))
+        self.assertFalse(isprime(6))
+        self.assertFalse(isprime(8))
+        self.assertFalse(isprime(9))
+        self.assertFalse(isprime(10))
+
+if __name__ == '__main__':
+    unittest.main()
+
